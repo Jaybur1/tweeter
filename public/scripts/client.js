4dffa418e28b282839b92f89755151178fe3af9e
Jaybur1 <jayburbyga@gmail.com>
2020-03-25 09:02:35 -0400

    added jQuary.timeago cdn to handle the timestamp to be formated as n time ago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,11 +5,54 @@
  */
 //a function that converts time stamp into how long ago .-level-1
 // import TimeAgo from 'javascript-time-ago';
+//////////////////////////////////////////////////////////////////////////////////
 
+const testData = [
+  {
+    user: {
+      name: "Bob",
+      avatars:
+        "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Spongebob-512.png",
+      handle: "@pants**2"
+    },
+    content: {
+      text:
+        "I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready!!!!!!!!!!"
+    },
+    created_at: 1584968238
+  },
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac"
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants"
+    },
+    created_at: 1569416238
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd"
+    },
+    content: {
+      text: "Je pense , donc je suis"
+    },
+    created_at: 1427288238
+  }
+];
 
+////////////////////////////////////////////////////////////////////////////////
 // a function that creates all the elements for the tweet and gluing all together
 const createTweetElement = tweet => {
+  //main tweet container
   let $tweet = $("<artical>").addClass("tweet");
+
+  //header to containe the user info ,avatar and the hendler
   let $header = $("<header>");
   let $userInfo = $("<div>").addClass("user");
   let $avatar = $("<img>")
@@ -19,14 +62,20 @@ const createTweetElement = tweet => {
   let $handle = $("<span>")
     .addClass("handle")
     .text(tweet.user.handle);
+
+  //container to contain the user content
   let $content = $("<p>")
     .addClass("tweet-content")
     .text(tweet.content.text);
+
+  //under line to diveide the contetnt with the footer
   let $hr = $("<hr />");
+
+  //footer to contain created_at and social-collection icons
   let $footer = $("<footer>");
   let $timePosted = $("<span>")
     .addClass("time-posted")
-    .text(jQuery.timeago(tweet.created_at));
+    .text(jQuery.timeago(tweet.created_at * 1000));
   let $socialCollection = $("<div>").addClass("social-collection").html(`
   <i class="fas fa-flag"></i>
   <i class="fas fa-retweet"></i>
@@ -34,7 +83,6 @@ const createTweetElement = tweet => {
   `);
 
   //glue everything
-
   $tweet.append([
     $header.append([$userInfo.append([$avatar, $userName]), $handle]),
     $content,
@@ -42,55 +90,16 @@ const createTweetElement = tweet => {
     $footer.append([$timePosted, $socialCollection])
   ]);
 
+  //return the glued whole glued element
   return $tweet;
 };
 
-
 $(document).ready(() => {
-  const testData = [{
-    user: {
-      name: "Bob",
-      avatars: "https://cdn0.iconfinder.com/data/icons/kameleon-free-pack-rounded/110/Spongebob-512.png",
-      handle: "@pants**2"
-    },
-    content: {
-      text: "I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready, I'm ready!!!!!!!!!!"
-    },
-    created_at:1461116232227
-  },
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac"
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants"
-      },
-      created_at: 1461116232227
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd"
-      },
-      content: {
-        text: "Je pense , donc je suis"
-      },
-      created_at: 1461113959088
-    }
-  ];
-
-  
   const renderTweets = tweets => {
-    tweets.forEach(tweet => $("#tweets-list").append(createTweetElement(tweet)));
+    tweets.forEach(tweet =>
+      $("#tweets-list").append(createTweetElement(tweet))
+    );
   };
-  
 
-
-  renderTweets(testData); 
+  renderTweets(testData);
 });
-
- 
